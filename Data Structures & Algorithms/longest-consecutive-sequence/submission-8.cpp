968901039e57e50a82910db601dce243532903e6
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-07-15 15:19:34 +0800

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    unordered_map<int, int> root;
+    int longestConsecutive(vector<int>& nums) {
+        for (const int& x : nums) {
+            root[x] = x;
+        }
+        
+        for (const int& x : nums) {
+            if (root.count(x + 1)) unite(x, x + 1);
+        }
+
+        int maxLen = 0;
+        for (const int& x : nums) {
+            // cout << x << " " << root[x] << endl;
+            maxLen = max(maxLen, find(x) - x + 1);
+        }
+
+        return maxLen;
+    }
+    void unite(int x, int y){
+        int root_x = find(x);
+        int root_y = find(y);
+        if (root_x != root_y) root[root_x] = root_y;
+    }
+    int find(int x){
+        return (root[x] != x) ? root[x] = find(root[x]) : root[x];
+    }
+};
