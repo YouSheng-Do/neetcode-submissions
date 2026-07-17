100568d8ca1850e2fab796338d881ca35cad191a
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-07-17 18:20:11 +0800

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = prices[0];
+        int diff = 0;
+
+        for (int p : prices) {
+            minimum = min(p, minimum);
+            diff = max(diff, p - minimum);
+        }
+
+        return diff;
+    }
+};
