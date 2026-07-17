27299a01de5e2b2942cf5178264e7f2dca7da058
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-07-17 16:46:00 +0800

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // [-2, -2, -1, -1, 2, 3] target = 1
+        //           ^   ^  ^   
+
+        // sort to make sure that same numbers are adjacent
+        
+        // Time : O(n^2)
+        // Spcace : O(n)
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for (int i = 0; i < n - 2; ++i) {
+            // duplicate case
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int target = -nums[i];
+            
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+                if (sum == target) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while (k > j && nums[k] == nums[k + 1]) {
+                        --k;
+                    }
+                }
+                else if (sum > target) {
+                    k--;
+                }
+                else {
+                    j++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
