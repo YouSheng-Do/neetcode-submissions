a10f34ea9c1107f1ac7b4865ae56e4f590481187
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-07 17:27:42 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        const int n = position.size();
+        vector<pair<int, double>> cars(n);
+        for (int i = 0; i < n; ++i) {
+            cars[i] = {target - position[i], (double) (target - position[i]) / speed[i]};
+        }
+
+        sort(cars.begin(), cars.end());
+
+        stack<double> mono_st;
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            double t = cars[i].second;
+            if (mono_st.empty() || t > mono_st.top()) {
+                mono_st.push(t);
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
