47b5b8e54a948747ea37298c48a45d986075d335
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-09 23:05:52 +0800

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binary_search(nums, 0, nums.size() - 1, target);
+    }
+    int binary_search(vector<int>& nums, int l, int r, int target) {
+        if (l > r) return -1;
+        int m = l + (r - l) / 2;
+        if (nums[m] == target) return m;
+        return (nums[m] > target) ? binary_search(nums, l, m - 1, target) : binary_search(nums, m + 1, r, target);
+    }
+};
