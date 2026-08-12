c5ff071b4d736d07f1605c29d1c9798bdb8a7d9a
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-13 00:12:50 +0800

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cpp
@@ -0,0 +1,110 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // two pass
+        // find median of nums1 and binary search on nums2
+        // select index i in nums1 and find insertion pos index j in nums2
+        // duplicate number case
+        // check i + j 
+        // if m + n is odd; => check i + j == (m + n) / 2;
+        // else => find (m + n) / 2 and (m + n) / 2 - 1, return the avg of two number
+        // if we cannot find median from nums1, that means the median is in nums2
+        // so the same thing oppositely
+
+        int l = 0, r = nums1.size() - 1;
+
+        int median = (nums1.size() + nums2.size()) / 2;
+
+        if ((nums1.size() + nums2.size()) % 2) {
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                int new_index = m + binarySearch1(nums1[m], nums2);
+                if (new_index == median) return nums1[m];
+                if (new_index > median) r = m - 1;
+                else l = m + 1;
+            }
+
+            l = 0, r = nums2.size() - 1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                int new_index = m + binarySearch2(nums2[m], nums1);
+                if (new_index == median) return nums2[m];
+                if (new_index > median) r = m - 1;
+                else l = m + 1;
+            }
+        }
+        else {
+            cout << "here" << endl;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                int pos = binarySearch1(nums1[m], nums2);
+                int new_index = m + pos;
+                cout << m << " " << pos << " " << new_index << endl;
+                if (new_index == median - 1) {
+                    int nums1_next = (m < nums1.size() - 1) ? nums1[m + 1] : INT_MAX;
+                    int nums2_next = (pos < nums2.size()) ? nums2[pos] : INT_MAX;
+                    cout << nums1_next << endl;
+                    cout << nums2_next << endl;
+                    return (double) (nums1[m] + min(nums1_next, nums2_next)) / 2.0;
+                }
+                if (new_index == median) {
+                    int nums1_prev = (m > 0) ? nums1[m - 1] : INT_MIN;
+                    int nums2_prev = (pos > 0) ? nums2[pos - 1] : INT_MIN;
+                    return (double) (nums1[m] + max(nums1_prev, nums2_prev)) / 2.0;
+                }
+                if (new_index > median) r = m - 1;
+                else l = m + 1;
+            }
+
+            cout << "there" << endl;
+            l = 0, r = nums2.size() - 1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                int pos = binarySearch2(nums2[m], nums1);
+                int new_index = m + pos;
+                if (new_index == median - 1) {
+                    int nums1_next = (pos < nums1.size()) ? nums1[pos] : INT_MAX;
+                    int nums2_next = (m < nums2.size() - 1) ? nums2[m + 1] : INT_MAX;
+                    return (double) (nums2[m] + min(nums1_next, nums2_next)) / 2.0;
+                }
+                if (new_index == median) {
+                    int nums1_prev = (pos > 0) ? nums1[pos - 1] : INT_MIN;
+                    int nums2_prev = (m > 0) ? nums2[m - 1] : INT_MIN;
+                    return (double) (nums2[m] + max(nums1_prev, nums2_prev)) / 2.0;
+                }
+                if (new_index > median) r = m - 1;
+                else l = m + 1;
+            }
+        }
+
+        return -1.0;
+    }
+    int binarySearch1(int target, vector<int>& nums) {
+        // return the insertion pos
+        int l = -1, r = nums.size();
+        // -inf, nums[0], nums[1], .........nums[n - 1], inf
+        // T,............................................F
+        // find the last true, and return l + 1
+        while (l + 1 < r) {
+            int m = l + (r - l) / 2;
+            if (target >= nums[m]) l = m;
+            else r = m;
+        }
+
+        return l + 1;
+    }
+    int binarySearch2(int target, vector<int>& nums) {
+        // return the insertion pos
+        int l = -1, r = nums.size();
+        // -inf, nums[0], nums[1], .........nums[n - 1], inf
+        // T,............................................F
+        // find the last true, and return l + 1
+        while (l + 1 < r) {
+            int m = l + (r - l) / 2;
+            if (target > nums[m]) l = m;
+            else r = m;
+        }
+
+        return l + 1;
+    }
+};
