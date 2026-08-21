707a99d737ffa3729b045b33bfd6bb5e7787561b
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-22 01:06:37 +0800

    Add: find-duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-8.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // binary search
+        // sort
+        // check nums[i] with index i
+        // nums[i] >= i + 1 => correct
+        // find the first incorrect value
+        // set l = -1, r = nums.size() - 1;
+        // make nums[l] is always correct and nums[r] is always incorrect
+        // while (l + 1 < r) 
+        // return r
+        //   [1,1,2,3]
+
+        // T,       F
+        // if nums[i] >= i + 1 => dupicate number index > i => l = i
+        // if nums[i] < i + 1 => too many number < nums[i] => duplicate number index < i => r = i
+        sort(nums.begin(), nums.end());
+        int l = -1, r = nums.size();
+
+        while (l + 1 < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= m + 1) {
+                l = m;
+            }
+            else {
+                r = m;
+            }
+        }
+
+        return r;
+    }
+};
