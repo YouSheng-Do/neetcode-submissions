b6b20f07a0b8bdd591ddf98bc30c0f0843ae6066
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-26 17:20:27 +0800

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // divide this prolbem into reverse k-group linked list to the end
+        // seperate each k-length linkedlist
+        // reverse all of them
+        // connect it back
+        // how to get k-lenght linked list
+        // =>fast & slow pointer
+
+        if (k == 1) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* cur = &dummy;
+
+        while (slow && fast) {
+            int times = k;
+            while (--times && fast->next) {
+                fast = fast->next;
+            }
+            if (times != 0) {
+                cur->next = slow;
+                break;
+            }
+            ListNode* next = fast->next;
+            cur->next = reverse(slow, fast);
+            cur = slow;
+            slow = next;
+            fast = next;
+        }
+
+        return dummy.next;
+    }
+
+    ListNode* reverse(ListNode* cur, ListNode* end) {
+        if (cur == end) return cur;
+        
+        ListNode* head = reverse(cur->next, end);
+        cur->next->next = cur;
+        cur->next = nullptr;
+        
+        return head;
+    }
+};
