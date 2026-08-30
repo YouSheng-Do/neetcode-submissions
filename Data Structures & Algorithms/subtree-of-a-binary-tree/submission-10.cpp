74c5a7c36710cb9c3ca813ea8358acd727fb0961
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-08-30 21:30:07 +0800

    Add: subtree-of-a-binary-tree - submission-10

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-10.cpp
@@ -0,0 +1,105 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // serialization of root and subRoot
+        // build two vector<string> to represent the two tree
+        // how to dot it ? 
+        // => ues "#" to represent nullptr. Traverse two trees in same traversal(ex: preorder)
+        // so now we'll have s_root and s_subRoot
+        // question becomes to find if s_subRoot is a substring in s_root
+        // =>we can solve it by KMP algorithm
+
+        // serialization two trees to string
+        vector<string> s_root, s_subRoot;
+        serialize(root, s_root);
+        serialize(subRoot, s_subRoot);
+
+        // find s_subRoot is a contiguous pattern in s_root
+        // => use z-function to solve it in linear time
+        
+        return z_function(s_root, s_subRoot);
+    }
+
+    void serialize(TreeNode* node, vector<string>& s) {
+        if (!node) {
+            s.push_back("#");
+            return;
+        }
+
+        s.push_back(to_string(node->val));
+        serialize(node->left, s);
+        serialize(node->right, s);
+    }
+
+    bool z_function(vector<string>& text, vector<string>& pattern) {
+        // pattern should be first, since we check prefix in combined
+        vector<string> combined = pattern;
+
+        combined.push_back("|");
+
+        for (auto& s : text) combined.push_back(s);
+
+        const int n = combined.size();
+        const int m = pattern.size();
+
+        // build a z-array 
+        // z[i] represent the length of the longest string from i that match the prefix of the whole string
+        // so if there's any z[i] == pattern.size(), return true;
+
+        vector<int> z(n, 0);
+
+        // naive way to build z-array
+        // O(n^2)
+        // for (int i = 1; i < n; ++i) {
+        //     while (i + z[i] < n && 
+        //         combined[z[i]] == combined[i + z[i]]) {
+        //         z[i]++;
+        //         if (z[i] == m) return true;
+        //     }
+        // }
+
+        // use z-box [l, r] represent that z[l....r] is the same with z[0....r-l]
+        // O(m + n)
+
+        int l = 0, r = 0;
+
+        for (int i = 1; i < n; ++i) {
+            // [l....i.....r]
+            // => s[l...i] is the same with prefix s[0...i-l]
+            // we want to know z[i] and because we know s[i....r] is equal to s[i-l...r-l], we can reuse z[i - l]
+            // z[i - l] represent the longest start from i-l match to prefix
+            // however, z[i - l] may exceed r boundary.
+            // so we use min()
+            if (i <= r) {
+                z[i] = min(z[i - l], r - i + 1);
+            }
+
+            while (i + z[i] < n &&
+                combined[i + z[i]] == combined[z[i]]) {
+                    z[i]++;
+                    if (z[i] == m) return true;
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+
+        return false;
+    }
+
+
+};
