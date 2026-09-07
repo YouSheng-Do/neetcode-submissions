61393e30534b83ff28b24bb2d5d1ef5c793724f1
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-09-08 01:24:47 +0800

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-4

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-4.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        // preorder has the root at first
+        // so we can find it in inorder and split inorder into two substree of this node
+
+        // divide and conquer
+        // find index in inorder
+        // get the range of left subtree and right subtree
+        // build node
+
+        unordered_map<int, int> nodeToPos;
+
+        int sz = inorder.size();
+        for (int i = 0; i < sz; ++i) 
+            nodeToPos[inorder[i]] = i;
+
+        int index = 0;
+        return buildNode(preorder, 0, sz - 1, index, nodeToPos);
+    }
+
+    TreeNode* buildNode(vector<int>& preorder, int l, int r, int& i, unordered_map<int, int>& nodeToPos) {
+        if (l > r) return nullptr;
+
+
+        // preorder : root -> left -> right
+        // build root node based on int i
+        int rootVal = preorder[i++];
+        TreeNode* root = new TreeNode(rootVal);
+
+        // find root node in inorder
+        int pos = nodeToPos[rootVal];
+
+        // construct leftsubtree
+        root->left = buildNode(preorder, l, pos - 1, i, nodeToPos);
+
+        root->right = buildNode(preorder, pos + 1, r, i, nodeToPos);
+
+        return root;
+    }
+};
