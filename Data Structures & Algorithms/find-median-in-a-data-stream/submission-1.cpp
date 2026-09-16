70317471bf001128fe183c6e87a064f0a2eaac86
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-09-17 02:16:01 +0800

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,36 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap; // smaller half
+    priority_queue<int, vector<int>, greater<int>> minHeap; // larger half
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        // add number to larger half
+        // if (minHeap.size() > maxHeap.size()) 
+        // pop from minHeap and add it to maxHeap
+        minHeap.push(num);
+
+        // maintain size
+        // minHeap.size() <= maxHeap.size()
+        if (minHeap.size() > maxHeap.size()) {
+            auto top = minHeap.top(); minHeap.pop();
+            maxHeap.push(top);
+        }
+
+        // check that if we have to swap or not
+        if (!minHeap.empty() && minHeap.top() < maxHeap.top()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (minHeap.size() == maxHeap.size()) 
+            return (double) (minHeap.top() + maxHeap.top()) / 2.0;
+        return (double) maxHeap.top();
+    }
+};
