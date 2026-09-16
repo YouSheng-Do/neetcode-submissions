7169efe3e199b5684b27f631ec0ad60905deb855
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-09-17 02:43:26 +0800

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        generate(nums, 0, cur, ans);
+
+        return ans;
+    }
+    void generate(vector<int>& nums, int i, vector<int>& cur, vector<vector<int>>& ans) {
+        if (i == nums.size()) {
+            ans.push_back(cur);
+            return;
+        }
+        generate(nums, i + 1, cur, ans);
+        
+        cur.push_back(nums[i]);
+        generate(nums, i + 1, cur, ans);
+        cur.pop_back();
+    }
+};
