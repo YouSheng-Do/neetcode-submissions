3f7a57e240c8ddfb18c2e21ea621b432ecb7def6
杜祐陞 <58830729+YouSheng-Do@users.noreply.github.com>
2026-09-20 17:16:45 +0800

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // sort
+        sort(candidates.begin(), candidates.end());
+
+        // backtracking
+        vector<vector<int>> ans;
+        vector<int> cur;
+
+        backtrack(cur, 0, candidates, target, ans);
+
+        return ans;
+    }
+    void backtrack(vector<int>& cur, int start, const vector<int>& nums, int target, vector<vector<int>>& ans) {
+        // if (target == 0) ans.push_back(cur); return;
+        // if i out of the boundary => return
+        // if nums[i] > target => return
+
+        if (target == 0) {
+            ans.push_back(cur);
+            return;
+        }
+
+        if (start >= nums.size() || nums[start] > target) return;
+
+        for (int i = start; i < nums.size(); ++i) {
+            // Skip duplicates at the same recursion level
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > target) return;
+
+            // pick nums[i]
+            cur.push_back(nums[i]);
+            backtrack(cur, i + 1, nums, target - nums[i], ans);
+            cur.pop_back();
+        }
+
+    }
+};
